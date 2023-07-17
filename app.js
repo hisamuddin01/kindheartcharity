b7efa375e4cbb0e69febb557a95b75cf58a07f0b
hisamuddin01 <hisamuddin357545@gmail.com>
2023-07-17 16:46:10 +0530

    navbar done

---
+++ app.js
@@ -0,0 +1,5 @@
+const menu = document.querySelector(".menu");
+const navItems = document.querySelector(".nav-items");
+menu.addEventListener('click', function(){
+    navItems.classList.toggle("show-items")
+})
