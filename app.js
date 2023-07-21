ef5731c4f77832706e704c9e5ea3a19983a49b93
hisamuddin01 <hisamuddin357545@gmail.com>
2023-07-21 12:10:41 +0530

    update feature section

--- app.js
+++ app.js
@@ -3,3 +3,23 @@ const navItems = document.querySelector(".nav-items");
 menu.addEventListener('click', function(){
     navItems.classList.toggle("show-items")
 })
+
+// const sliderWrappr = document.querySelector(".slider-wrapper");
+// const sliderItem = document.querySelectorAll(".sliderItem")
+
+// const btnPrev= document.querySelector(".btn-prev");
+// const btnNext= document.querySelector(".btn-next");
+
+// btnPrev.addEventListener('click', ()=> {
+//     console.log("clicked")
+// })
+// btnNext.addEventListener('click', ()=> {
+//     console.log("clicked")
+// })
+
+// let currentSlide = 0;
+
+// function handleNext(){
+//     currentSlide += 1;
+//     sliderWrappr.style.transform = `translateX(${currentSlide * -100})`
+// }
